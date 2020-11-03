507e60b37d294a31cf076f0028d2ca5a0da23994
Sunny Nazar <sn@cloudeteer.de>
2020-11-03 19:32:33 +0100

    modified the address CIDR

--- terraform/azure/environment/dev/vnet.tf
+++ terraform/azure/environment/dev/vnet.tf
@@ -1,13 +1,13 @@
 module "network" {
   # VNET definitions
   source              = "../../modules/network"
-  vnet_name           = "vnet-cdtdemo-test-weu-10.90.00.0_24"
+  vnet_name           = "vnet-cdtdemo-test-weu-10.90.0.0_24"
   resource_group_name = module.RG.resource_groups["tfstate"].name
   location            = var.location
-  vnet_address_space  = ["10.90.00.0/24"]
+  vnet_address_space  = ["10.90.0.0/24"]
   # SUBNET definitions 
-  subnet_names             = ["vnet-cdtdemo-test-weu-10.90.00.0_25", "AzureBastionSubnet"]
-  subnet_prefixes          = ["10.90.00.0/25", "10.90.00.224/27"]
+  subnet_names             = ["vnet-cdtdemo-test-weu-10.90.0.0_25", "AzureBastionSubnet"]
+  subnet_prefixes          = ["10.90.0.0/25", "10.90.0.224/27"]
   subnet_service_endpoints = ["Microsoft.Sql"]
   tags                     = var.tags
 }
